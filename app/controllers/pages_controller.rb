5d27d3613394fb2522ff9dbc507538139282e8d8
horaciocoronel <arielc@gmail.com>
2017-10-11 14:28:58 -0400

    Add instance variables on pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,12 +2,15 @@ class PagesController < ApplicationController
 
   def welcome
     #render :about #overrides route
+    @header = "This is a new header"
     render :welcome
   end
 
   def about
+    @header = "This is a new header for About"
   end
 
   def contest
+    @header = "Congratulations! You're a winner"
   end
 end
