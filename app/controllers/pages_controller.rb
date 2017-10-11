bf0df1257166c1fad0270a1b29398c709a406caf
horaciocoronel <arielc@gmail.com>
2017-10-11 14:48:05 -0400

    Add before_action to avoid DRY

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,7 +13,16 @@ class PagesController < ApplicationController
   def contest
     @header = "Congratulations! You're a winner"
   end
+
+  before_action :set_kitten_url, only: [:kitten, :kittens]
+
   def kitten
+  end
+
+  def kittens
+  end
+
+  def set_kitten_url
     requested_size = params[:size]
     @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
   end
